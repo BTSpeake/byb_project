4bf6648e11f950d2d6c36b44bf9ead8db02bbbfd
Bspeake <benjaminspeake@gmail.com>
2023-02-17 12:39:38 +0000

    Initial Commit

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+def helloWorld():
+    print("Hello World!")
+    return 
+
+def gitMessage():
+    print("Git is awesome!")
+    return 
+
+if __name__ == "__main__":
+    # helloWorld()
+    gitMessage()
